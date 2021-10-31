f387183d11b0afb533307f4618ce74430fc5073f
bundleofivan <bundleofivan@gmail.com>
2021-10-31 01:14:59 -0700

    added configurable port for tcp connection

--- src/hitagi.rs
+++ src/hitagi.rs
@@ -1,10 +1,25 @@
+use std::net::{TcpListener, TcpStream};
+
 pub struct Hitagi {
     port: String,
 }
 
+fn handle_client(stream: TcpStream) {
+    println!("handling connection");
+}
+
+fn open_connection(port: String) -> std::io::Result<()> {
+    let listener = TcpListener::bind(format!("localhost:{}", port))?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+
 impl Hitagi {
     pub fn init(port: String) -> Hitagi {
-        println!("hello");
-        return Hitagi { port: port };
+        open_connection(port.clone());
+        Hitagi { port: port }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use std::net::{TcpListener, TcpStream};
 mod hitagi;
-use crate::hitagi::Hitagi;
+use hitagi::Hitagi;
 
-fn handle_client(stream: TcpStream) {
-    println!("hello world");
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
-    Hitagi::init("123".to_string());
-
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
-    Ok(())
+fn main() {
+    let server: hitagi::Hitagi = Hitagi::init("3000".to_string());
 }
